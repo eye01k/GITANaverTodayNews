2c37bc708743a88318792532014895b8b882336f
eye01k <dmlduf0113@naver.com>
2022-03-30 18:23:43 +0900

    aa

--- BallonPJ/JS/main.js
+++ BallonPJ/JS/main.js
@@ -8,7 +8,39 @@ function chgMv(mvid, start){
     tg.src = `https://www.youtube.com/embed/${mvid}?autoplay=1&playsinline=1&start=${start}`
 }
 
-function chgImg(){
+// function chgImg(){
+//     // 대상: .plist a
+//     let plist = document.querySelectorAll(".plist img");
+//     plist.onclick = console.log(plist.length);
+//     // 변경대상: #Pimg
+//     let Pimg = document.querySelector("#Pimg");
+//     // 변경내용: #Pimg에 img 요소 넣기
 
-}
+// }
 
+window.addEventListener("load", ()=>{
+
+    let plist = document.querySelectorAll(".plist img")
+    // console.log(plist.length);
+    let pimg = document.querySelector("#Pimg");
+
+    // console.log(plist);
+
+    let data;
+
+    for(let x of plist){
+        // console.log(x.alt);
+        x.onclick = ()=>{
+            switch(x.alt){
+                case "메시": data = 01; break;
+                case "레반도프스키": data = 02; break;
+                case "조르지뉴": data = 03; break;
+                case "벤제마": data = 04; break;
+                case "캉테": data = 05; break;
+                case "호날두": data = 06; break;
+            }
+            console.log(data);
+            pimg.innerHTML = `<img src="images/Big0${data}.png">`;
+        }; // click 함수 //
+    } // for문///
+});/// 로드구역 ///
