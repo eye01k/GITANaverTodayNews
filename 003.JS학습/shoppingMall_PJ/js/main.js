84678ac8c1a9a5affcdf46f37470caca32939cf0
eye01k <dmlduf0113@naver.com>
2022-04-06 12:21:25 +0900

    Update main.js

--- 003.JS학습/shoppingMall_PJ/js/main.js
+++ 003.JS학습/shoppingMall_PJ/js/main.js
@@ -12,18 +12,13 @@ window.addEventListener("load", () => {
         4. 기능흐름: 
         (1) 오른쪽버튼 클릭시 다음슬라이드가
         나타나도록 슬라이드박스의 left값을
-        -100% 단위의 배수로 이동시킨다!
-        -> 이동시 한계값은 마지막 슬라이드
-        이동 배수인 4, 즉 -400%임
-        (2) 왼쪽버튼 클릭시 이전슬라이드가
-        나타나도록 left값은 -100% 단위
-        배수중 이전 배수가 되도록 변경함
-        -> 이동시 한계값은 첫번째 슬라이드의
-        위치값인 0을 기준한다
-        (3) 처음이전과 끝 다음 슬라이드는
-        처음과 마지막 한계값에서 연결하여
-        구현한다.(처음은 마지막으로 마지막은
-        처음으로 돌아가게 함)
+        -100%로 이동시킨다!
+        ->이 때, 바깥에 나가있는 첫 번째 슬라이드 li를 잘라서 맨 뒤로 보낸다
+        동시에 left 값을 0으로 변경
+
+        (2) 왼쪽버튼 클릭시 
+        먼저 맨 뒤의 슬라이드 li를 맨 앞으로 이동시킨다. 이 때 left 값을 -100%로 변경
+        이후 left 값을 0으로 변경하여 애니메이션
 
         5. 추가기능: 슬라이드 위치표시 블릿
         - 블릿대상 : .indic li
@@ -31,5 +26,54 @@ window.addEventListener("load", () => {
         li에 클래스 "on"주기(나머진 빼기-초기화)
 
     ************************************/
+    // 이벤트 대상: .abtn
+    let abtn = document.querySelectorAll(".abtn");
+
+    // 변경대상: #slide
+    let slide = document.querySelector("#slide");
+
+    /***************** 
+        함수명: goSlide
+        기능: 슬라이드를 왼쪽/오른쪽으로 이동
+    *****************/
+
+        const goSlide = dir => { // dir: 이동 방향(1-오른쪽, 0-왼쪽)
+
+            // 전달값 및 호출 확인
+            console.log("전달값: ",dir)
+
+            // 방향 분기
+            // 2-1. 오른쪽 버튼
+            if(dir){ // dir===1 이면 true
+
+                // (1) 슬라이드박스의 left값을 -100%로 이동시킨다!
+                slide.style.left = "-100%";
+                slide.style.transition = ".6s ease-out";
+
+                // (2) 바깥에 나가있는 첫 번째 슬라이드 li를 잘라서 맨 뒤로 보낸다
+                // 일정시간 후 한 번 실행하는 함수 - setTimeout
+                setTimeout(()=>{
+                    // appendChild(요소) - 선택요소 맨 뒤 이동
+                    slide.appendChild(slide.querySelectorAll("li")[0]);
+
+                    // (3) 동시에 left 값을 0으로 변경
+                    slide.style.left = "0";
+                    slide.style.transition = "none";
+                },600); ///// setTimeout
+
+                
+            } /////// if
+            // 2-2. 왼쪽 버튼
+            else{
+
+            } /////// else
+
+        }; /////// goSlide
+
+    // 오른쪽 버튼 클릭 시
+    abtn[1].onclick = () => goSlide(1);
+
+    // 왼쪽 버튼 클릭 시
+    abtn[0].onclick = () => goSlide(0);
 
 }); ///////////// load //////////////////////
