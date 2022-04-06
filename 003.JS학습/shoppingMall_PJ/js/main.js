4c9ad680b9c78f795e138ea5b8b6dd0d493f24ce
eye01k <dmlduf0113@naver.com>
2022-04-06 14:39:16 +0900

    aa

--- 003.JS학습/shoppingMall_PJ/js/main.js
+++ 003.JS학습/shoppingMall_PJ/js/main.js
@@ -32,6 +32,23 @@ window.addEventListener("load", () => {
     // 변경대상: #slide
     let slide = document.querySelector("#slide");
 
+    // 변경대상: .indic li
+    let indic = document.querySelectorAll(".indic li");
+
+
+    // 슬라이드 li에 순번 속성 주기
+    // li가 잘려서 이동하므로 순서가 항상 변경되기 때문에 고유한 순번을 지정하기 위함
+    // 지정방법: li에 data-속성명 으로 사용자 정의 속성을 준다
+    // (사용자 정의 속성은 data- 로 시작하는 속성명이다)
+    // 한번만 실행하는 재귀호출 함수 -> (함수)()
+    (()=>{
+        // 대상: #slide li
+        let tg = slide.querySelectorAll("li");
+        for(let i = 0; i<tg.length; i++){
+            tg[i].setAttribute("data-seq",i);
+        }
+    })();
+
     /***************** 
         함수명: goSlide
         기능: 슬라이드를 왼쪽/오른쪽으로 이동
@@ -85,12 +102,31 @@ window.addEventListener("load", () => {
                 },10); //// setTimeout
             } /////// else
 
+            // 3. 슬라이드 순번과 동일한 순번의 블릿에 클래스 on 넣기
+            // 모두 class="on"을 없애고 슬라이드 li의 data-seq의 숫자를 읽어서 블릿 li의 해당 순번에 class="on"을 준다 
+    
+            // 초기화(class="on" 지우기)
+            for(let x of indic) x.classList.remove("on");
+    
+            // 슬라이드의 data-seq 값을 읽어옴
+            // 오른쪽 버튼 이동일 경우 두 번째 슬라이드[1]
+            // 왼쪽 버튼 이동일 경우 첫 번째 슬라이드[0]
+            // => 버튼 구분번호도 오른쪽 1, 왼쪽 0 -> dir 변수
+            let seq = sli[dir].getAttribute("data-seq");
+            // getAttribute(속성명) -> 속성값 읽어오기
+            // setAttribute(속성명,속성값) -> 새로운 속성값 셋팅
+    
+            // 해당 순번에 class="on" 넣기
+            indic[seq].classList.add("on");
         }; /////// goSlide
+
 
     // 오른쪽 버튼 클릭 시
     abtn[1].onclick = () => goSlide(1);
 
     // 왼쪽 버튼 클릭 시
     abtn[0].onclick = () => goSlide(0);
+
+    
 
 }); ///////////// load //////////////////////
