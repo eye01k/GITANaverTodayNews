d367dc707364cb0652f84a75b10906731143d408
eye01k <dmlduf0113@naver.com>
2022-04-07 09:31:26 +0900

    Update main.js

--- PlabPJ/JS/main.js
+++ PlabPJ/JS/main.js
@@ -1,11 +1,14 @@
 window.addEventListener("DOMContentLoaded", ()=>{
 
-    let tg = document.querySelectorAll(".gnb>ul>li>a")
+    // let tg = document.querySelectorAll(".gnb>ul>li>a")
     let sm = document.querySelectorAll("li.sm");
     
-    tg[0].onclick = ()=>{
-        sm[0].classList.add("open");
-    }; //// click //////
+    for(let x of sm){
+        x.querySelector("a").onclick = ()=>{
+            x.classList.add("open");
+        };
+    }
+
 
     let appbx = document.querySelector(".apply");
 
