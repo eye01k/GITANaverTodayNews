c98afa7450a9364002e882ebefef6c55506ebfb2
eye01k <dmlduf0113@naver.com>
2022-04-12 10:17:26 +0900

    Update main.js

--- 004.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
+++ 004.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
@@ -8,8 +8,17 @@ $(()=>{
         대상1: 버튼들 - .btns>button
     */
    let btns = $(".btns>button");
+   // console.log(btns.length);
 
-//    console.log(btns.length);
+   // 대상2: 미니언즈 - .mi
+   let mi = $(".mi");
+   
+   // 대상3: 빌딩 각 방 - .building li
+   let bd = $(".building li");
+   // console.log(bd.length);
+   
 
+    $(btns).click(function() {
 
+    }); /////// click
 }); ////////////////// JQB
