32a0abd23a44844b027e8f1d7a516d3caafc0003
eye01k <dmlduf0113@naver.com>
2022-04-12 10:08:32 +0900

    aa

--- 004.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
+++ 004.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
@@ -1,1 +1,15 @@
 // 제이쿼리 기본 JS - main.js 
+
+$(()=>{
+    // console.log("로딩완료");
+
+    /* 
+        1. 대상선정 변수 할당
+        대상1: 버튼들 - .btns>button
+    */
+   let btns = $(".btns>button");
+
+//    console.log(btns.length);
+
+
+}); ////////////////// JQB
