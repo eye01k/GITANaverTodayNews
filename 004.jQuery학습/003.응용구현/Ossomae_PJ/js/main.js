ed58ea38e5781a9f9fefbd77fe14df0c08459530
eye01k <dmlduf0113@naver.com>
2022-04-14 10:37:39 +0900

    aa

--- 004.jQuery학습/003.응용구현/Ossomae_PJ/js/main.js
+++ 004.jQuery학습/003.응용구현/Ossomae_PJ/js/main.js
@@ -6,9 +6,16 @@ $(()=>{
     // 변경대상: .gbx -> 갤러리 부모박스
     let gbx = $(".gbx");
 
+    // 광클 금지 상태 변수
+    let prot = 0; // 0-허용 1-막기
+
     // 양쪽 이동버튼 클릭 시
     // 이벤트 대상: .abtn
     $(".abtn").click(function(){
+
+        if(prot) return;
+        prot = 1; // 잠금
+        setTimeout(()=>prot=0,400); // .4s 후에 잠금 해제
 
         console.log("오른쪽이냐?",$(this).is(".rb"));
 
@@ -21,5 +28,22 @@ $(()=>{
             $(gbx).append(gbx.find("div").first());
         }
         else $(gbx).prepend(gbx.find("div").last());
+
+        // 자동넘김 지우기
+        clearInterval(autoI);
     }); //////////////////버튼 클릭
+
+    // 인터발용 변수
+    let autoI;
+    // 자동넘김 함수
+    const autoCall = ()=>{
+        autoI = setInterval(()=>{
+            // $(".rb").trigger("click");
+            // trigger(이벤트) -> 이벤트 강제 발생
+
+            $(gbx).append(gbx.find("div").first());
+        },2000) //// autoI 할당
+    }; ///////// autoCall
+
+    autoCall(); ////// 자동넘김 함수 최초호출
 }); ///////////////////////////JQB
