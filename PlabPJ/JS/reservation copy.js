fe1af1082c7c5fb2e8793ab0431d7c43a6e0658c
Tom B. Parker <tombap8@hotmail.com>
2022-04-14 17:42:29 +0900

    ss

--- PlabPJ/JS/reservation copy.js
+++ PlabPJ/JS/reservation copy.js
@@ -10,17 +10,23 @@ $(()=>{
     let week =["일", "월", "화", "수", "목", "금", "토"]
     console.log(today.getDay());
 
+    // 테스트 날짜 변경
+    addDay.setDate(addDay.getDate()+15);
+
 
     $(".week p").each((idx,ele)=>{
         // console.log(idx);
-        idx = idx;
-        addDay.setDate(idx);
+        if(idx===0)
+        addDay.setDate(addDay.getDate());
+        else        
+        addDay.setDate(addDay.getDate()+1);
         // setDate(오늘날짜+날수) -> 변경된 날짜가됨!
 
         // -> 날짜를 더해서 다음달과 경계날짜가 잘나오는지 살펴봄!
 
         // addDay에 날짜를 더해서 아래에서 getDate()로 날짜 추출!
-        $(ele).text(addDay.getDate())
+        // $(ele).text(addDay.getDate())
+        $(ele).text(addDay.toLocaleDateString())
         // p태그 다음 형제인 span태그에 요일 넣기!
         // (더해진 날짜의 요일을 바로구해옴!)
         .next().text(week[addDay.getDay()]);
