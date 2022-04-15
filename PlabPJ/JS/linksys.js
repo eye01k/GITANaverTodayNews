abdd4d1bf187971b4d9de09f4f942b06668bf603
eye01k <dmlduf0113@naver.com>
2022-04-15 11:18:02 +0900

    aa

---
+++ PlabPJ/JS/linksys.js
@@ -0,0 +1,8 @@
+// Plab PJ 링크 시스템 JS
+
+$(()=>{
+    $(".logo").click(()=>{
+        // console.log("dd");
+        location.href = "index.html";
+    }); // 로고 클릭 시 메인페이지로
+}); ///////////////////JQB
