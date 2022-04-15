56562b15a7b08b77192f522c245a0ecd428c73d9
eye01k <dmlduf0113@naver.com>
2022-04-15 17:03:29 +0900

    Update login.js

--- 009.Ajax학습/002.site/js/login.js
+++ 009.Ajax학습/002.site/js/login.js
@@ -1,16 +1,16 @@
 // Vogue PJ 로그인 페이지 JS
 
-$(()=>{
+$(() => {
 
     // console.log("로딩완료");
-    
+
     // 대상1: 아이디 입력요소
     let mid = $("#mid");
     // 대상2: 비밀번호 입력요소
     let mpw = $("#mpw");
 
     // 로그인 버튼 클릭 시 
-    $("#sbtn").click(function(e){
+    $("#sbtn").click(function (e) {
 
         // 1. 기본 이동 막기
         e.preventDefault();
@@ -18,7 +18,7 @@ $(()=>{
         // 2. 아이디/비밀번호 빈 값 여부 확인
         // val() 입력된 값 읽어오기
         // trim() 앞뒤 공백 제거
-        if(mid.val().trim()==="" || mpw.val().trim()=== ""){
+        if (mid.val().trim() === "" || mpw.val().trim() === "") {
             alert("아이디와 비밀번호를 확인하세요");
             mid.val("").focus(); // 아이디 지우고 포커스 감
             mpw.val(""); // 비번 지우기
@@ -36,16 +36,33 @@ $(()=>{
 
                 // 2. 보낼 데이터
                 {
-                    "mid" : mid.val(), // 아이디
-                    "mpw" : mpw.val() // 비밀번호
+                    "mid": mid.val(), // 아이디
+                    "mpw": mpw.val() // 비밀번호
                 },
 
                 // 3. 처리 후 함수
-                function(res){
-                    console.log("결과: ",res);
+                function (res) {
+                    console.log("결과: ", res);
+
+                    // 3-1. 로그인 성공 시
+                    if (res === "ok") {
+                        alert("로그인에 성공하였습니다!");
+                    } ////////// if: 로그인 성공 시
+
+                    // 3-2. 아이디가 없는 경우
+                    else if(res === "no"){
+                        alert("존재하지 않는 아이디입니다");
+                        mid.val("").focus();
+                        mpw.val("");
+                    } ///////// else if: 아이디가 없는 경우
+
+                    // 3-3. 비밀번호 불일치일 경우
+                    else if(res === "again"){
+                        alert("비밀번호가 일치하지 않습니다");
+                        mpw.val("").focus();
+                    } ///////// else if: 아이디가 없는 경우
                 } ///// callback
             ); /////////////// post
-            // alert("로그인에 성공하였습니다!");
         } ////////////// else
 
     }); ///////// click
